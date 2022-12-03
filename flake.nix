b941acb7c04619f6bf5c006768b8a215838283fe
Tobias Brumhard <code@brumhard.com>
2022-12-03 11:10:33 +0100

    Init with flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "local development setup";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            vhs
+            go
+          ];
+
+          shellHook = "";
+        };
+      }
+    );
+}
